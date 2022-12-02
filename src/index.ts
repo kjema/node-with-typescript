d0a74c62cdc996ce0ee3a6dd7ac4e7de2fd56b62
Marcus Kjell <marcus.kjell@gmail.com>
2022-12-02 21:38:54 +0100

    cosmos wip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-import { helper } from "./helper.js";
-import helloWorld from "./helper.cjs";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-helper();
-console.log(helloWorld);
+import { CosmosClient } from "@azure/cosmos";
+import users from "./data/users.json" assert { type: "json" };
+import helloWorld from "./helper.cjs";
+import { helper } from "./helper.js";
+
+const key = process.env.COSMOS_KEY || "<cosmos key>";
+const endpoint = process.env.COSMOS_ENDPOINT || "<cosmos endpoint>";
+const containerId = process.env.COSMOS_CONTAINER || "<cosmos container>";
+const databaseId = process.env.COSMOS_DATABASE || "<cosmos database>";
+
+const client = new CosmosClient({ endpoint, key });
+
+async function run(): Promise<void> {
+  const { database } = await client.databases.createIfNotExists({
+    id: databaseId,
+  });
+  const { container } = await database.containers.createIfNotExists({
+    id: containerId,
+  });
+
+  console.log(helloWorld);
+  helper();
+  console.log(users);
+}
+
+try {
+  await run();
+} catch (error) {
+  if (error instanceof Error) console.log(error.message);
+  else console.log(error);
+}
